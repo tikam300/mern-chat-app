e28e19c9fba8ad528537e0cc02fa51ae1140f0db
Tikam Singh <tikamsingh2402@gmail.com>
2024-05-13 20:15:26 +0530

    Ui  part is done. starting socket onwards

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import Conversation from './Conversation'
+import useGetConvesations from '../../hooks/useGetConvesations'
+import { getRandomEmoji } from '../../utils/emojis';
 
 const Conversations = () => {
+   const {loading,conversations} = useGetConvesations();
+   console.log("CONVERATIONS: ", conversations);
   return (
     <div className='py-2 flex flex-col overflow-auto'>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
+        
+      {conversations.map((conversation,idx)=>(
+        <Conversation 
+          key={conversation._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastIdx={idx === conversations.length-1 }
+        />
+      ))}
+
+        {loading? <span className='loading loading-spinner mx-auto'></span>:null}
     </div>
   )
 }
