c6cfabcb7ec47d469a4b52d392bd5f438e9132ec
Tikam Singh <tikamsingh2402@gmail.com>
2024-05-14 23:42:51 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 // const app = express();
 const PORT= process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -21,10 +24,11 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
-// app.get("/",(req,res)=>{
-//     // root route http://localhost:5000/
-//     res.send("Hello World!");
-// });
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+});
 
 
 
